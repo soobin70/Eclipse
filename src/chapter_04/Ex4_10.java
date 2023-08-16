08ada3039e5cfa146ee8af2bc8d9380d0458ceed
tnqls <tnqls@LAPTOP-JKTJU38M>
2023-08-17 00:51:02 +0900

    chapter_04

---
+++ src/chapter_04/Ex4_10.java
@@ -0,0 +1,18 @@
+package chapter_04;
+
+public class Ex4_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num =12345;
+		int sum = 0;
+		while(num>0) {
+			sum+=num%10;
+			num/=10;
+		}
+		
+		System.out.println("sum="+sum);
+
+	}
+
+}
