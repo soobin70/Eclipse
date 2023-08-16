55edc4dc5ffb10128da13d00b7a8170d1fea8a9f
tnqls <tnqls@LAPTOP-JKTJU38M>
2023-08-17 01:02:45 +0900

    chapter_05

---
+++ src/chapter_05/Ex5_2.java
@@ -0,0 +1,19 @@
+package chapter_05;
+
+public class Ex5_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][] arr= {
+				{5, 5, 5, 5, 5},
+				{10, 10, 10},
+				{20, 20, 20, 20},
+				{30, 30}
+				
+		};
+		
+		
+
+	}
+
+}
